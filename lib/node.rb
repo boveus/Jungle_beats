c1ba33e83ba5e129f1c70f9ac137db8fff43d894
Brandon Stewart <boveus@gmail.com>
2017-07-02 12:39:27 -0600

    Working correctly, now.  Everything actually traverses the linked list now instead of my node_list array.  Need to finish pop.

--- lib/node.rb
+++ lib/node.rb
@@ -1,40 +1,23 @@
 class Node
   attr_reader :data,
-              :next_node,
-              :headnode,
-              :index
+              :next_node
+
 
   def initialize(data)
     @data = data
-    @headnode = false
     @next_node = nil
-    @previous_node = nil
-    @index = nil
-    # @head_node = head_node
+  end
+
+  def is_last_node
+    @next_node = nil
   end
 
   def set_next_node(next_node)
     @next_node = next_node
   end
 
-  def set_previous_node(previous_node)
-    @previous_node = previous_node
+  def set_next_node_nil
+    @next_node = nil
   end
 
-  def is_headnode
-    @headnode = true
-    @index = 0
-    @previous_node = nil
-  end
-
-  def change_index(index)
-    if index != 0
-      @headnode = false
-    end
-    @index = index
-  end
-
-  def change_data(new_data)
-    @data = new_data
-  end
 end
